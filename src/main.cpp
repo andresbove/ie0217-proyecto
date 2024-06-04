1c57bb6c6db6d4424e3a2c970406bd450d7d07cc
Bonilla Vega <andres.bonilla.vega@intel.com>
2024-06-03 19:27:28 -0600

    Añadiendo submenus. 'Atencion a cliente', 'Informacion general sobre prestamos bancarios',  y los de los préstamos

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,11 @@
 
 using namespace std;
 
+void menuAtencionCliente();
+void menuInformacionPrestamos();
+void menuSacarPrestamo();
+void menuPagarPrestamo();
+
 int main() {
     int opcion;
 
@@ -24,12 +29,10 @@ int main() {
 
             switch (opcion) {
             case 1:
-                cout << "Ha seleccionado atencion a cliente." << endl;
-                // Ir a menu de atencion de cliente
+                menuAtencionCliente();
                 break;
             case 2:
-                cout << "Informacion general sobre prestamos bancarios." << endl;
-                // Ir a menu de atencion de cliente
+                menuInformacionPrestamos();
                 break;
             case 3:
                 cout << "Saliendo del programa..." << endl;
@@ -37,9 +40,7 @@ int main() {
             default:
                 cout << "Opcion no valida, intente de nuevo." << endl;
             }
-        }
-
-        catch (const exception& e) {
+        } catch (const exception& e) {
             cerr << e.what() << endl;
             cin.clear(); // Limpiar error
             cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Descartar opcion ingresada
@@ -48,4 +49,172 @@ int main() {
     } while (opcion != 3);
 
     return 0;
+}
+
+void menuAtencionCliente() {
+    int opcion;
+    do {
+        cout << "\n--------------MENU-----------------------------" << endl;
+        cout << "Atencion a cliente" << endl;
+        cout << "        1. Sacar prestamo" << endl;
+        cout << "        2. Pagar Prestamo" << endl;
+        cout << "        3. Generar informe (.txt)" << endl;
+        cout << "        4. Volver" << endl;
+        cout << "--------------MENU-----------------------------" << endl;
+        cout << "Ingrese su opcion: ";
+
+        try {
+            cin >> opcion;
+
+            if (cin.fail()) {
+                throw runtime_error("Opcion no valida, debe ingresar un numero del 1 al 4.\n");
+            }
+
+            switch (opcion) {
+            case 1:
+                menuSacarPrestamo();
+                break;
+            case 2:
+                menuPagarPrestamo();
+                break;
+            case 3:
+                cout << "Ha seleccionado generar informe." << endl;
+                break;
+            case 4:
+                cout << "Volviendo al menu principal..." << endl;
+                break;
+            default:
+                cout << "Opcion no valida, intente de nuevo." << endl;
+            }
+        } catch (const exception& e) {
+            cerr << e.what() << endl;
+            cin.clear(); // Limpiar error
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Descartar opcion ingresada
+        }
+
+    } while (opcion != 4);
+}
+
+void menuSacarPrestamo() {
+    int opcion;
+    do {
+        cout << "\n--------------MENU-----------------------------" << endl;
+        cout << "Sacar prestamo" << endl;
+        cout << "        1. Prestamo en Dolares" << endl;
+        cout << "        2. Prestamo en Colones" << endl;
+        cout << "        3. Volver" << endl;
+        cout << "--------------MENU-----------------------------" << endl;
+        cout << "Ingrese su opcion: ";
+
+        try {
+            cin >> opcion;
+
+            if (cin.fail()) {
+                throw runtime_error("Opcion no valida, debe ingresar un numero del 1 al 3.\n");
+            }
+
+            switch (opcion) {
+            case 1:
+                cout << "Ha seleccionado sacar prestamo en Dolares." << endl;
+                break;
+            case 2:
+                cout << "Ha seleccionado sacar prestamo en Colones." << endl;
+                break;
+            case 3:
+                cout << "Volviendo al menu de Atención a Cliente..." << endl;
+                break;
+            default:
+                cout << "Opcion no valida, intente de nuevo." << endl;
+            }
+        } catch (const exception& e) {
+            cerr << e.what() << endl;
+            cin.clear(); // Limpiar error
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Descartar opcion ingresada
+        }
+
+    } while (opcion != 3);
+}
+
+void menuPagarPrestamo() {
+    int opcion;
+    do {
+        cout << "\n--------------MENU-----------------------------" << endl;
+        cout << "Pagar prestamo" << endl;
+        cout << "        1. Prestamo en Dolares" << endl;
+        cout << "        2. Prestamo en Colones" << endl;
+        cout << "        3. Volver" << endl;
+        cout << "--------------MENU-----------------------------"<<"\n" << endl;
+        cout << "Ingrese su opcion: ";
+
+        try {
+            cin >> opcion;
+
+            if (cin.fail()) {
+                throw runtime_error("Opcion no valida, debe ingresar un numero del 1 al 3.\n");
+            }
+
+            switch (opcion) {
+            case 1:
+                cout << "Ha seleccionado pagar prestamo en Dolares." << endl;
+                break;
+            case 2:
+                cout << "Ha seleccionado pagar prestamo en Colones." << endl;
+                break;
+            case 3:
+                cout << "Volviendo al menu de Atención a Cliente..." << endl;
+                break;
+            default:
+                cout << "Opcion no valida, intente de nuevo." << endl;
+            }
+        } catch (const exception& e) {
+            cerr << e.what() << endl;
+            cin.clear(); // Limpiar error
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Descartar opcion ingresada
+        }
+
+    } while (opcion != 3);
+}
+
+void menuInformacionPrestamos() {
+    int opcion;
+    do {
+        cout << "\n--------------MENU-----------------------------" << endl;
+        cout << "Informacion general sobre prestamos bancarios" << endl;
+        cout << "        1. Prestamo Personal" << endl;
+        cout << "        2. Prestamo Prendario" << endl;
+        cout << "        3. Prestamo Hipotecario" << endl;
+        cout << "        4. Volver" << endl;
+        cout << "--------------MENU-----------------------------" << endl;
+        cout << "Ingrese su opcion: ";
+
+        try {
+            cin >> opcion;
+
+            if (cin.fail()) {
+                throw runtime_error("Opcion no valida, debe ingresar un numero del 1 al 4.\n");
+            }
+
+            switch (opcion) {
+            case 1:
+                cout << "Ha seleccionado Prestamo Personal." << endl;
+                break;
+            case 2:
+                cout << "Ha seleccionado Prestamo Prendario." << endl;
+                break;
+            case 3:
+                cout << "Ha seleccionado Prestamo Hipotecario." << endl;
+                break;
+            case 4:
+                cout << "Volviendo al menu principal..." << endl;
+                break;
+            default:
+                cout << "Opcion no valida, intente de nuevo." << endl;
+            }
+        } catch (const exception& e) {
+            cerr << e.what() << endl;
+            cin.clear(); // Limpiar error
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Descartar opcion ingresada
+        }
+
+    } while (opcion != 4);
 }
